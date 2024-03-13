dde30e2539a0ff5f7c698cb3b3c62d7e3c1cd90d
drakula <filip.sobczuk@o2.pl>
2024-03-13 10:11:55 +0100

    added more messages improved comments

--- src/PICalculator.java
+++ src/PICalculator.java
@@ -13,6 +13,8 @@ public class PICalculator {
     BigDecimal suma = new BigDecimal(0, mc);
 
     int iloscIteracji = 10000; // ilosc wykonań pętli
+
+    // obliczanie pi używając wzoru Gregory-Leibniz'a
     for (int i = 1; i <= iloscIteracji; i++) {
       BigDecimal wyraz = new BigDecimal(4, mc).divide(new BigDecimal(2 * i - 1, mc), mc);
 
@@ -23,7 +25,6 @@ public class PICalculator {
       }
     }
 
-    // Wypisuje wartość liczby pi z podaną dokładnością obliczeń
     System.out.println("Wartość liczby pi z dokładnością do " + precyzja + " miejsc po przecinku: \n" + suma);
   }
 }
