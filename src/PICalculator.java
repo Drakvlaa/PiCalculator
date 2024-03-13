81461e3574c59c35d22fc65b2f71658d11153b89
Drakvlaa <Filip.sobczuk@o2.pl>
2024-03-13 07:31:38 +0100

    initial commit

---
+++ src/PICalculator.java
@@ -0,0 +1,29 @@
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.Scanner;
+
+public class PICalculator {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int precyzja = scanner.nextInt(); // dokładność liczby PI
+    MathContext mc = new MathContext(precyzja);
+
+    BigDecimal suma = new BigDecimal(0, mc);
+
+    int iloscIteracji = 10000; // ilosc wykonań pętli
+
+    for (int i = 1; i <= iloscIteracji; i++) {
+      BigDecimal wyraz = new BigDecimal(4, mc).divide(new BigDecimal(2 * i - 1, mc), mc);
+
+      if (i % 2 == 0) {
+        suma = suma.subtract(wyraz, mc);
+      } else {
+        suma = suma.add(wyraz, mc);
+      }
+    }
+
+    // Wypisuje wartość liczby pi z podaną dokładnością obliczeń
+    System.out.println("Wartość liczby pi z dokładnością do " + precyzja + " miejsc po przecinku: \n" + suma);
+  }
+}
