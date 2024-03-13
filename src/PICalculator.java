111da17ba272e6100b24a9db70432e1bf757cf62
drakula <filip.sobczuk@o2.pl>
2024-03-13 10:09:35 +0100

    added more messages improved comments

--- src/PICalculator.java
+++ src/PICalculator.java
@@ -1,4 +1,3 @@
-
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.util.Scanner;
@@ -6,13 +5,14 @@ import java.util.Scanner;
 public class PICalculator {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    System.out.println("podaj dokładność z jaką chcesz obliczyć liczbę PI:");
     int precyzja = scanner.nextInt(); // dokładność liczby PI
+    scanner.close();
     MathContext mc = new MathContext(precyzja);
 
     BigDecimal suma = new BigDecimal(0, mc);
 
     int iloscIteracji = 10000; // ilosc wykonań pętli
-
     for (int i = 1; i <= iloscIteracji; i++) {
       BigDecimal wyraz = new BigDecimal(4, mc).divide(new BigDecimal(2 * i - 1, mc), mc);
 
